78919198f012bd135668cedb270e58addf424ace
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-10-14 16:14:25 +0300

    :tada: react-query setup done

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import TestComponent from "./components/testComponent"
 
 function App() {
 
+  const queryClient = new QueryClient();
+
   return (
-    <TestComponent/>
+    <QueryClientProvider client={queryClient}>
+      <TestComponent/>
+    </QueryClientProvider>
   )
 }
 
