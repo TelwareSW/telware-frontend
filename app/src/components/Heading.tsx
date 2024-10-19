1986496513d4f7af2ded990eacef51ddfd1f6d5a
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-10-20 00:56:01 +0300

    :lipstick: Adding more options to Heading component

--- app/src/components/Heading.tsx
+++ app/src/components/Heading.tsx
@@ -22,6 +22,31 @@ const Heading = styled.h1`
       font-weight: 500;
     `}
 
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 1.25rem;
+      font-weight: 450;
+    `}
+  
+    ${(props) =>
+    props.as === "h5" &&
+    css`
+      font-size: 1rem;
+      font-weight: 400;
+    `}
+
+    ${(props) =>
+    props.as === "h6" &&
+    css`
+      font-size: 1rem;
+      font-weight: 400;
+      color: var(--color-text-secondary) !important;
+      padding-left: 5%;
+      padding-right: 5%;
+      padding-bottom: 0.5rem;
+    `}
+
   color: var(--color-text);
   line-height: 1.4;
 `;
