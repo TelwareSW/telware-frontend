eb10538f91513d939b8b90df0180305b52381687
Amir Kedis <88613195+amir-kedis@users.noreply.github.com>
2024-10-25 01:45:35 +0300

    feat(profile-settings): add edit bio, screen name, and username to edit profile page + some DX improvements (#9)
    * feature: add edit profile settings component
    
    * fix(global): fix import error in jest
    
    * feat(profile-settings): implement the core of profile settings
    
    * test(profile-settings): add mock for users/me

---
+++ app/src/features/profile-settings/schema/EditProfileSchema.ts
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const BIO_MAX_LENGTH = 120;
+
+const ValidationSchema = yup.object({
+  profilePicture: yup.string(),
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string(),
+  bio: yup.string().max(BIO_MAX_LENGTH, "Bio must be 160 characters or less"),
+  username: yup
+    .string()
+    .required("Username is required")
+    .min(5, "Username must be at least 5 characters")
+    .matches(/^[a-zA-Z0-9_]*$/, "Username must be alphanumeric"),
+});
+
+export { ValidationSchema, BIO_MAX_LENGTH };
