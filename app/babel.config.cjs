619e39568e8de88b160c1f8ee39f3989df92e8d3
amir-kedis <amir.kedis@gmail.com>
2024-10-25 00:17:59 +0300

    fix(global): fix import error in jest

--- app/babel.config.cjs
+++ app/babel.config.cjs
@@ -1,7 +1,27 @@
+const config = require("./tsconfig.app.json");
+
+const alias = Object.entries(config["compilerOptions"]["paths"]).reduce(
+  (acc, [key, value]) => {
+    acc[key.replace("/*", "")] = value[0]
+      .replace("/*", "")
+      .replace("./", "./src/");
+    return acc;
+  },
+  {},
+);
+
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { esmodules: true } }],
     ["@babel/preset-react", { runtime: "automatic" }],
     "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias,
+      },
+    ],
   ],
 };
