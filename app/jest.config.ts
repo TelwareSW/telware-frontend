52fab462f76368f72e9f430800ec31e27bfe0963
amir-kedis <amir.kedis@gmail.com>
2024-10-24 10:43:07 +0300

    feat(global): jest common config

---
+++ app/jest.config.ts
@@ -0,0 +1,23 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!**/vendor/**"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  transform: {
+    ".(ts|tsx)": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/mocks/fileMock.ts",
+  },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+    "reportWebVitals.ts",
+    "jest.setup.ts",
+    "index.tsx",
+  ],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
