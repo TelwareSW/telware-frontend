304e8479555402c176a644d861dc980d528ad739
Amir Kedis <88613195+amir-kedis@users.noreply.github.com>
2024-10-24 11:51:53 +0300

    [COMMON] Floating Label Input Components & small DX improvements (#8)
    * feat(global): add css snippet to disable drag and select (feels more like an app)
    
    * feat(common): create Floating Label Input Component
    
    * chore(global): setup paths aliases for easier more maintanable imports
    
    * feat(common): add form dev tools examples
    
    * feat(global): jest common config
    
    * test(common): add tests for floating label input

---
+++ app/src/features/profile-settings/NameAndBioForm.tsx
@@ -0,0 +1,12 @@
+import FloatingLabelInputDemo from "@components/inputs/float_label_input/FloatingLabelInputDemo";
+import styled from "styled-components";
+
+const SettingContainer = styled.div`
+  padding: 1rem 1.5rem;
+`;
+
+function NameAndBioForm() {
+  return <FloatingLabelInputDemo />;
+}
+
+export default NameAndBioForm;
---
+++ app/src/features/profile-settings/UserNameForm.tsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function UserNameForm() {
+  return <div></div>;
+}
+
+export default UserNameForm;
