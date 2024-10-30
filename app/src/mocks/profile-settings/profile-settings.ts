b82ef4cdf9860c331d31afa8e0406f3bed5f946c
amir-kedis <amir.kedis@gmail.com>
2024-10-30 18:09:49 +0300

    feat(profile-settings): add mock handler for profile settings again

---
+++ app/src/mocks/profile-settings/profile-settings.ts
@@ -0,0 +1,30 @@
+import { http, HttpResponse } from "msw";
+import { MOCK_USER } from "@mocks/mockData";
+
+export const profileSettingsMock = [
+  http.get(/.*\.(png|jpg|jpeg|gif|svg)$/, async () => {
+    return undefined;
+  }),
+
+  http.get("/users/me", async () => {
+    return HttpResponse.json(
+      {
+        status: "success",
+        data: MOCK_USER,
+      },
+      { status: 200 },
+    );
+  }),
+
+  http.patch("/users/me", async ({ request }) => {
+    const newProfileSettings = await request.json();
+
+    return HttpResponse.json(
+      {
+        status: "success",
+        data: newProfileSettings,
+      },
+      { status: 200 },
+    );
+  }),
+];
