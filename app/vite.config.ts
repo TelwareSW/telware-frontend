a5907e9766f19e8a84af05048ac3450a0c26b0fc
Amir Kedis <88613195+amir-kedis@users.noreply.github.com>
2024-10-30 11:50:43 +0300

    fix: fix docker compose not working and add guide to how to run docker (#22)

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: { port: 5174 },
-  plugins: [react(), tsconfigPaths()],
-});
+
+export default ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
+  return defineConfig({
+    server: { port: parseInt(process.env.VITE_PORT) },
+    plugins: [react(), tsconfigPaths()],
+  });
+};
