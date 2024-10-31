3159bc89b607389531287aa71545d6ab1b3eeaae
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-10-31 21:38:21 +0300

    :wrench: (msw): conditional enable

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,20 +3,23 @@ import { createRoot } from "react-dom/client";
 
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
+import { ENV } from "@constants";
 
 import App from "./App.tsx";
 
 async function enableMocking() {
-  const { worker } = await import("./mocks/browser");
-  return worker.start();
+  if (ENV === "development" || ENV === "test") {
+    const { worker } = await import("./mocks/browser");
+    return worker.start();
+  }
 }
 
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     // <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
     // </StrictMode>
   );
 });
