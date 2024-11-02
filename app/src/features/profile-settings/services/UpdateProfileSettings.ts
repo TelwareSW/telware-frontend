eca78dbfdb49348ddd07af8fd600c2d9b6b994c0
Amir Kedis <88613195+amir-kedis@users.noreply.github.com>
2024-11-02 20:05:49 +0200

    fix(ops): backend integration with cors and cookies (#33)

--- app/src/features/profile-settings/services/UpdateProfileSettings.ts
+++ app/src/features/profile-settings/services/UpdateProfileSettings.ts
@@ -8,6 +8,7 @@ async function UpdateProfileSettings(newProfileSettings: EditProfileForm) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newProfileSettings),
+    credentials: "include",
   });
 
   const data = await res.json();
