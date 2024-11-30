e31b9492199c5acb2c4e865cf5efc08cd3b43281
asmaa-204 <asmaa.abozaid204@gmail.com>
2024-11-30 13:03:14 +0200

    🚨 Removed some warnings

--- app/src/features/chats/hooks/useMessageSender.ts
+++ app/src/features/chats/hooks/useMessageSender.ts
@@ -1,14 +1,12 @@
 import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { addMessage } from "@state/messages/messages";
 import { RootState } from "@state/store";
 
 import { useSocket } from "@hooks/useSocket";
 
 export const useMessageSender = () => {
   const { sendMessage, editMessage } = useSocket();
-  const dispatch = useDispatch();
   const userId = useSelector((state: RootState) => state.user.userInfo.id);
   const activeMessage = useSelector((state: RootState) => state.activeMessage);
   const { chatId } = useParams<{ chatId: string }>();
