d9118d3849a35e534f0385a6f9201ae2db4de064
asmaa-204 <asmaa.abozaid204@gmail.com>
2024-12-24 00:54:53 +0200

    removed react query devtool from ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import GlobalStyles from "./styles/GlobalStyles";
@@ -43,7 +42,6 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <Toaster />
       <GlobalStyles />
       <BrowserRouter>
